b0f7cd70600cc6a2510c93181eefff58077a53ab
itsutkarsha <kavitakeutkarsha09@gmail.com>
2026-04-14 11:36:53 +0530

    Added AboutUs component

---
+++ src/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>About Paradise Nursery</h1>
+      
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination for beautiful and healthy houseplants. 
+        We are passionate about bringing nature closer to your home by offering a wide variety of indoor 
+        and outdoor plants.
+      </p>
+
+      <p>
+        Our mission is to promote a greener lifestyle and make plant care easy and enjoyable for everyone. 
+        Whether you are a beginner or a plant enthusiast, Paradise Nursery has something for you.
+      </p>
+
+      <p>
+        Explore our collection and bring home the joy of greenery!
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
